00da97f8631351a5629b0ff846324e9ac357e2a9
Jens <soppafisk@gmail.com>
2015-10-08 12:43:08 +0200

    nu kan man lägga till personer

--- script.js
+++ script.js
@@ -1,5 +1,67 @@
 (function(){
+  // personklass
+  var Person = function(name) {
+    this.name = name;
+  };
+
+  // todoklass
+  var Todo = function() {
+
+  };
+
+  // tar hand om local storage
+  var Model = function() {
+    this.persons = [];
+    this.todos = [];
+    this.getNamesToString = function() {
+      var array = [];
+      for(var i = 0; i< this.persons.length; i++) {
+        array.push(this.persons[i].name);
+        console.log(this.persons[i].name);
+      }
+      return array.join();
+    }
+  };
+
+  var TodoApp = function() {
+    var _this = this;
+    var model = new Model();
+
+    var addPerson = function() {
+      var name = addForm.person.value;
+      console.log("adda person");
+      var person = new Person(name);
+      console.log(model);
+      model.persons.push(person);
+      updateNames();
+
+    }
+    var addPersonButton = document.querySelector("#addPersonButton");
+
+    addPersonButton.addEventListener("click", addPerson);
+
+    var addTodo = function() {
+      console.log("adda todo");
+      var todo = new Todo();
+
+    }
+    var addTodoButton = document.querySelector("#addTodoButton");
+
+    addTodoButton.addEventListener("click", addTodo);
+
+    var namesDiv = document.querySelector("#names");
+    var updateNames = function() {
+      namesDiv.innerHTML = model.getNamesToString();
+    }
+  }
 
 
+
+
+var run = function() {
+  var app = new TodoApp();
+}
+
+document.addEventListener("DOMContentLoaded", run);
 
 })(window);
