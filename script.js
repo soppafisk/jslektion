f6e062b00908f0b56547704f6645decf5366e43d
speckis <ervisfilipovic90@gmail.com>
2015-10-08 13:01:55 +0200

    hej

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 (function(){
 
+	var TodoApp = function() {
 
+	}
+
+var person = function() {
+
+	var addPerson = function() {
+		alert("adda");
+	}
+
+};
+
+var todo = function() {
+
+};
+
+var model = function() {
+
+};
+
+var app = new TodoApp();
 
 })(window);
